11a79eaffcaf7fcdc3829d7e85195a68c299ac21
kadranb <kadrak02@hotmail.com>
2022-12-16 13:33:55 -0300

    agora vai build

---
+++ docs/precache-manifest.c3b2f333a182c4f12b6ad8f95d8d128b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4009934e104d5c5b384184257abf5fa4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4922532a8d0ab2cfd0c1",
+    "url": "/static/css/main.b22424e6.chunk.css"
+  },
+  {
+    "revision": "345e7c090b4d82582ae6",
+    "url": "/static/js/2.a6dbb846.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a6dbb846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4922532a8d0ab2cfd0c1",
+    "url": "/static/js/main.7943cbfe.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.7943cbfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7c799cd51a8639aab9",
+    "url": "/static/js/runtime-main.1cb1a291.js"
+  },
+  {
+    "revision": "6c27fd4099d1760d1c8bab2e03ee04f0",
+    "url": "/static/media/login.6c27fd40.svg"
+  }
+]);
